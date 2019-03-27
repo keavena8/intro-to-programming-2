cd1fea927dd8f3741721dd23dea4df45086f8877
keavena8 <36245641+keavena8@users.noreply.github.com>
2019-03-27 14:28:37 +0000

    lab 10 q1

---
+++ lab 10 q1/Circle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10_q1
+{
+    class Circle
+    {
+        private int _radius;
+        private int _x = 1;
+        private int _y;
+
+        public int Radius
+        {
+            get
+            {
+                return _radius;
+
+            }
+            set
+            {
+                _radius = value;
+            }
+        }
+        public int x
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public int y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+        public Circle(int r, int x, int y)
+        {
+
+            _radius = r;
+            _x = x;
+            _y = y;
+        }
+        public double GetArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+        public override string ToString()
+        {
+            return "The radius of the circle is " + Radius;
+        }
+    }
+}
---
+++ lab 10 q1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10_q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle MyCircle = new Circle(5, 1, 10);
+            Console.WriteLine(MyCircle.ToString());
+            Console.WriteLine(MyCircle.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
