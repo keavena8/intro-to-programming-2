1187f6a2cf609d171d59b55e558074eefbbd555e
keavena8 <36245641+keavena8@users.noreply.github.com>
2019-03-27 14:34:33 +0000

    lab 10 q0( continue on from lectures)

---
+++ lab 10 q0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            employee myEmployee = new employee("Pat", "male", 10, "American");
+            Console.WriteLine(myEmployee.ToString());
+            Console.ReadLine();
+        }
+    }
+}
