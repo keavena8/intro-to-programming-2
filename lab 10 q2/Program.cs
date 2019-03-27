7c808a157a218f462718873d7ea2bad28bae6f2f
keavena8 <36245641+keavena8@users.noreply.github.com>
2019-03-27 15:22:54 +0000

    lab 10 q2(undone)

---
+++ lab 10 q2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10_q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car MyCar = new Car("Audi", "Blue", 8821);
+            Console.WriteLine(MyCar.ToString());
+            Console.ReadLine();
+             
+        }
+    }
+}
---
+++ lab 10 q2/car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10_q2
+{
+    class Car
+    {
+        private string _brand;
+        private string _colour;
+        private int _regNumber;
+
+        public string Brand
+        {
+           get
+           {
+                return _brand;
+           }
+           set
+           {
+                _brand = value;
+           }
+        }
+        public string Colour
+        {
+            get
+            {
+                return _colour;
+            }
+            set
+            {
+                _colour = value;
+            }
+        }
+        public int RegNumber
+        {
+            get
+            {
+                return _regNumber;
+            }
+            set
+            {
+                _regNumber = value;
+            }
+        }
+        public Car(string b, string c, int r)
+        {
+            _brand = b;
+            _colour = c;
+            _regNumber = r;
+
+            if (b == "Audi" || b == "BMW" || b == "VW")
+            {
+                Console.WriteLine("This is the correct car!");
+            }
+            else
+            {
+                Console.WriteLine("This car is incorrect! It can only be Audi, VW and BMW");
+            }
+        }
+        public override string ToString()
+        {
+            return "Car: " + Brand + "\nColour: " + Colour + "\nRegistration Number: " + RegNumber;
+        }
+
+    }
+}
