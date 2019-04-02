da17e1f9994002a5b1c056afec83ab79a2fce1a1
keavena8 <36245641+keavena8@users.noreply.github.com>
2019-04-02 07:34:29 +0100

    Update Program.cs

--- lab 10 q2/Program.cs
+++ lab 10 q2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +10,14 @@ namespace lab_10_q2
     {
         static void Main(string[] args)
         {
-            Car MyCar = new Car("Audi", "Blue", 8821);
+            Car MyCar = new Car("Audi", "Red", 981);
+            Car MyCar2 = new Car("Ford", "Blue", 8821);
+            Car MyCar3 = new Car("BMW", "Green", 1);
             Console.WriteLine(MyCar.ToString());
+            Console.WriteLine(MyCar2.ToString());
+            Console.WriteLine(MyCar3.ToString());
             Console.ReadLine();
-             
+
         }
     }
 }
