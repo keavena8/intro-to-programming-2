b951b2d19e8dd940c7ee1c720f94c43ed05bb2c7
keavena8 <36245641+keavena8@users.noreply.github.com>
2019-04-03 10:54:17 +0100

    Add files via upload

---
+++ lab 11q3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            computer comp1 = new computer("Dell", "Usa");
+            Console.WriteLine(comp1.Format());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab 11q3/computer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class computer
+    {
+        private string _company;
+        private string _country;
+        public string Company
+        {
+            get
+            {
+                return _company;
+            }
+            set
+            {
+                _company = value;
+            }
+        }
+        public string Country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                _country = value;
+            }
+        }
+        public computer(string com, string c)
+        {
+            _company = com;
+            _country = c;
+        }
+        public string Format()
+        {
+            return "Company: " + Company + "\nCountry: " + Country;
+        }
+        public override string ToString()
+        {
+            return Company.ToString() + Country.ToString();
+        }
+
+        class laptop : computer
+        {
+            private lapname;
+            private lapcolour;
+            public 
+        
+
+        }
+    }
+}
