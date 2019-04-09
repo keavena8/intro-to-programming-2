843711d4a59384ccb5537bc7d6e96a14fb7c7d98
keavena8 <36245641+keavena8@users.noreply.github.com>
2019-04-09 15:01:08 +0100

    Update computer.cs

--- lab 11q3/computer.cs
+++ lab 11q3/computer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,22 +37,97 @@ namespace ConsoleApplication7
             _company = com;
             _country = c;
         }
-        public string Format()
+        public virtual string Format()
         {
-            return "Company: " + Company + "\nCountry: " + Country;
+            return  Company  + Country;
         }
         public override string ToString()
         {
-            return Company.ToString() + Country.ToString();
-        }
-
-        class laptop : computer
-        {
-            private lapname;
-            private lapcolour;
-            public 
-        
-
+            return "Company: " + Company.ToString() + "\nCountry: " + Country.ToString();
         }
     }
+
+    class laptop : computer
+    {
+        private string _lapname;
+        private string _lapcolour;
+        public string lapname
+        { 
+            get
+            {
+                return _lapname;
+            }
+            set
+            {
+               _lapname = value;
+            }
+        }
+        public string lapcolour
+        {
+             get
+             {
+                return _lapcolour;
+             }
+             set
+             {
+                _lapcolour = value;
+             }
+        }
+        public laptop (string com, string c, string ln,string lc) : base(com, c)
+        {
+            _lapname = ln;
+            _lapcolour = lc;
+        }
+        public override string Format()
+        {
+            return  Company  + Country  + lapname + lapcolour;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "\nLapTop Name: " + lapname + "\nLaptop Colour: " + lapcolour;
+        }
+
+    }
+    class desktop : computer
+    {
+        private string _deskname;
+        private string _deskcolour;
+        public string deskname
+        {
+            get
+            {
+                return _deskname;
+            }
+            set
+            {
+                _deskname = value;
+            }
+
+        }
+        public string deskcolour
+        {
+            get
+            {
+                return _deskcolour;
+            }
+            set
+            {
+                _deskcolour = value;
+            }
+        }
+        public desktop (string com, string c, string dn, string dc) : base(com,c)
+        {
+            _deskname = dn;
+            _deskcolour = dc;
+        }
+        public override string Format()
+        {
+            return Company + Country + deskname + deskcolour;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "\nDesktop Name: " + deskname + "\nDesktop Colour: " + deskcolour; 
+        }
+    }
+    
 }
